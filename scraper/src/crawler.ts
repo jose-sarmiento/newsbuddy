77da0b390a24cbaf6419da3ee8e1de94c5418391
Jose Sarmiento <jose.sarmiento@media-meter.com>
2024-04-07 09:48:57 +0800

    feat(scraper): add random delay, rotating user agents between requests

--- scraper/src/crawler.ts
+++ scraper/src/crawler.ts
@@ -1,4 +1,3 @@
-import fs from "fs/promises";
 import {
     ArticleLinkT,
     ArticleT,
@@ -9,6 +8,8 @@ import {
 import { isWithinElapsed, isWithinRange } from "./utils/dates";
 import { normalizeUrl } from "./utils/urls";
 import logger from "./config/logger";
+import sleep from "./utils/sleep"; // Utility function to add delay between requests
+import { randomDelay } from "./utils";
 
 class Crawler {
     constructor(private publication: PublicationI) {}
@@ -110,8 +111,27 @@ class Crawler {
     }
 
     private async getHTMLBody(url: string): Promise<string | undefined> {
+        const maxDelay = 3000; // Maximum delay between requests in milliseconds
+        const minDelay = 10000;
+
+        const userAgentList = [
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
+        ];
+        const randomUserAgent =
+            userAgentList[Math.floor(Math.random() * userAgentList.length)];
         try {
-            const response: Response = await fetch(url);
+            await randomDelay(minDelay, maxDelay);
+            const response: Response = await fetch(url, {
+                headers: {
+                    "User-Agent": randomUserAgent,
+                },
+            });
             if (response.status > 399) {
                 logger.info(`error in fetch: ${url} ${response.status}`);
                 return;
@@ -126,7 +146,9 @@ class Crawler {
             const responseText = await response.text();
             return responseText;
         } catch (error) {
-            console.error(error);
+            if (error instanceof Error) {
+                console.error(error.message);
+            }
             return;
         }
     }
